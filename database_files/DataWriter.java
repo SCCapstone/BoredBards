e7689332e3319082569098a609d7047b73345599
csimons637 <csimons637@gmail.com>
2022-10-19 18:15:24 -0400

    Added database reader and writer and users.json; built out basic json structure for users

---
+++ database_files/DataWriter.java
@@ -0,0 +1,10 @@
+//json libraries
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+// java libraries
+import java.util.ArrayList;
+import java.util.HashMap;
+/**
+ * @author Charles Simons -csimons637 and Caroline Barrineau - CarolineBarrineau
+ */
