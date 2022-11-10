bf7c671ca7c8092090f6e8803c26fa42f07d4524
csimons637 <csimons637@gmail.com>
2022-11-10 15:08:46 -0500

    Created a test icon (number 1) for the app (see #60) and added it to AndroidManifest.xml

--- app/src/main/java/com/example/bored_bard/Campaign.java
+++ app/src/main/java/com/example/bored_bard/Campaign.java
@@ -49,7 +49,7 @@ import androidx.annotation.Nullable;
         // player attributes will need to be set by the UI, when added
         // users will enter the values manually, since we don't have a pdf scanner option (yet)
         Player p = new Player(type, name, charClass, race); // this only creates the base character; no attributes, modifiers, etc.
-        p.setAttributes(strengthMod, dexterityMod, constitutionMod, intelligenceMod, wisdomMod, charismaMod);
+        p.setAttributes(strength, dexterity, constitution, intelligence, wisdom, charisma);
         p.setModifiers(strengthMod, dexterityMod, constitutionMod, intelligenceMod, wisdomMod, charismaMod);
         this.playerList.add(p);
     }
