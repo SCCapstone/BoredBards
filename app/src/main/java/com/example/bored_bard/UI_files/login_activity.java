1c6609b056dfc42722f4c4b104fc35f8b1d6b3c9
CAROLINE BARRINEAU <caroline.barrineau@gmail.com>
2022-11-21 16:40:18 -0500

    reg/login Screen and java

---
+++ app/src/main/java/com/example/bored_bard/UI_files/login_activity.java
@@ -0,0 +1,24 @@
+// This controls the login_screen's buttons
+package com.example.bored_bard.UI_files;
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class login_activity extends AppCompatActivity {
+    private Button loginButton;
+    private EditText usernameET;
+    private EditText emailET;
+    private EditText passwordET;
+    private EditText repasswordET;
+
+    protected void onCreate(Bundle savedInstanceState) {
+       super.onCreate(savedInstanceState);
+
+    }
+
+
+
+}
