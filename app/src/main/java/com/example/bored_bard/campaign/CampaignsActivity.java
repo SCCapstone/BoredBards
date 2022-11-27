53a278ba6f5815ddf2c3986a3ad6f624f0de8719
csimons637 <csimons637@gmail.com>
2022-11-27 11:55:17 -0500

    Further work on the DieRoller, CampaignsActivity and layouts for same (#21, #74 and #75)

--- app/src/main/java/com/example/bored_bard/campaign/CampaignsActivity.java
+++ app/src/main/java/com/example/bored_bard/campaign/CampaignsActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.annotation.NonNull;
@@ -24,18 +25,29 @@ public class CampaignsActivity extends AppCompatActivity {
         bottomNavView.setOnNavigationItemSelectedListener(item -> {
             switch (item.getItemId()) {
                 case R.id.campaigns_page:
+                    startActivity(new Intent(getApplicationContext(), CampaignsActivity.class));
+                    bottomNavView.setSelectedItemId(R.id.campaigns_page);
                     return true;
                 case R.id.dice_page:
                     startActivity(new Intent(getApplicationContext(), DieRoller.class));
+                    bottomNavView.setSelectedItemId(R.id.dice_page);
                     return true;
                 case R.id.notes_page:
                     // start the notes activity here
+                    bottomNavView.setSelectedItemId(R.id.notes_page);
                     return true;
                 case R.id.settings_page:
                     // start the settings activity here
+                    bottomNavView.setSelectedItemId(R.id.settings_page);
                     return true;
             }
             return false;
         });
+
+        Button editStats = findViewById(R.id.editStats);
+        Button combat = findViewById(R.id.combat);
+
+        combat.setOnClickListener(v -> startActivity(new Intent(getApplicationContext(), CombatActivity.class)));
+        editStats.setOnClickListener(v -> startActivity(new Intent(getApplicationContext(), EditStats.class)));
     }
 }
