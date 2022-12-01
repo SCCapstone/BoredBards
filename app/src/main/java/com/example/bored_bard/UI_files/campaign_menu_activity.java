b9a3360d2cd5b6e52841ad06aaba993b67e498cb
csimons637 <csimons637@gmail.com>
2022-12-01 12:34:04 -0500

    Adding more functionality to activity files for demo (#97)

--- app/src/main/java/com/example/bored_bard/UI_files/campaign_menu_activity.java
+++ app/src/main/java/com/example/bored_bard/UI_files/campaign_menu_activity.java
@@ -1,4 +1,16 @@
 package com.example.bored_bard.UI_files;
 
-public class campaign_menu_activity {
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.bored_bard.R;
+
+public class campaign_menu_activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.campaign_menu_screen);
+    }
 }
