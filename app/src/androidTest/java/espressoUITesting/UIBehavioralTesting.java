8bca462add3526653b3b79a9db09bbf99f37910f
Caroline Barrineau <caroline.barrineau@gmail.com>
2023-01-30 21:37:41 -0500

    new package under androidTest

---
+++ app/src/androidTest/java/espressoUITesting/UIBehavioralTesting.java
@@ -0,0 +1,14 @@
+package espressoUITesting;
+
+import static androidx.test.espresso.Espresso.*;
+import static androidx.test.espresso.action.ViewActions.click;
+import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
+import static androidx.test.espresso.matcher.ViewMatchers.withText;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+public class UIBehavioralTesting {
+}
