db432d4aabaa52131511cd38df2dc7740d0c5c9d
Caroline Barrineau <caroline.barrineau@gmail.com>
2023-01-30 21:11:25 -0500

    committing and then moving to a new branch

---
+++ app/src/main/java/espressoUI/UIbehavioraltesting.java
@@ -0,0 +1,19 @@
+package espressoUI;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+@RunWith(AndroidJUnit4.class)
+public class UIbehavioraltesting {
+    @Before
+
+    @Rule
+
+
+    @Test
+    public void useAppContext() {
+
+
+    }
+}
