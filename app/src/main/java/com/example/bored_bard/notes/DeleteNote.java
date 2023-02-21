57b26b237cc6d0a8f751f19e8388efbf048f07ec
FrozenDrew <macmura@email.sc.edu>
2023-02-20 23:09:08 -0500

    Created the Activity Files for #128 and #129

---
+++ app/src/main/java/com/example/bored_bard/notes/DeleteNote.java
@@ -0,0 +1,14 @@
+package com.example.bored_bard.notes;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class DeleteNote extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_delete_note);
+    }
+}
