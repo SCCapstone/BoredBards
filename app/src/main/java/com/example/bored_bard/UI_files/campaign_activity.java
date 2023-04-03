62a2ab52804db614b007e26c012c1b9da87f8fab
Jake Powers <jpowers1324@gmail.com>
2023-04-02 22:41:07 -0400

    Partial updates to Encyclopedia class

--- app/src/main/java/com/example/bored_bard/UI_files/campaign_activity.java
+++ app/src/main/java/com/example/bored_bard/UI_files/campaign_activity.java
@@ -7,6 +7,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import com.example.bored_bard.R;
 import com.example.bored_bard.dice_roller.DieRoller;
+import com.example.bored_bard.encyclopedia.Encyclopedia;
 import com.example.bored_bard.notes.NotesMainActivity;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
@@ -40,7 +41,7 @@ public class campaign_activity extends AppCompatActivity {
                 overridePendingTransition(0, 0);
                 return true;
             } else if (item.getItemId() == R.id.settings_page) {
-                startActivity(new Intent(getApplicationContext(), settings_activity.class));
+                startActivity(new Intent(getApplicationContext(), Encyclopedia.class));
                 overridePendingTransition(0, 0);
                 return true;
             } else {
--- app/src/main/java/com/example/bored_bard/encyclopedia/Encyclopedia.java
+++ app/src/main/java/com/example/bored_bard/encyclopedia/Encyclopedia.java
@@ -1,22 +1,58 @@
 package com.example.bored_bard.encyclopedia;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.bored_bard.R;
 import com.example.bored_bard.UI_files.campaign_activity;
 import com.example.bored_bard.UI_files.settings_activity;
 import com.example.bored_bard.dice_roller.DieRoller;
 import com.example.bored_bard.notes.NotesMainActivity;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
-public class Encylcopedia extends AppCompatActivity {
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.Query;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+public class Encyclopedia extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_encylcopedia); //Tie java file to xml file
+        FirebaseFirestore db = FirebaseFirestore.getInstance();
+        //Gson gson = new Gson();
+
+
+        //CollectionReference ref = db.collection("Encyclopedia");
+        db.collection("Encyclopedia")
+                .get()
+                .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                        if(task.isSuccessful()) {
+                            for (QueryDocumentSnapshot document : task.getResult()) {
+                                Log.d("TAG", "Data " + " => " + document.getData());
+                            }
+                        } else {
+                            Log.d("TAG", "Error getting documents: " + task.getException());
+                        }
+                    }
+                });
+
+
+
+
+
 
         //Setup for Bottom Nav Menu
         BottomNavigationView bottomNavView = findViewById(R.id.bottom_nav);
@@ -36,7 +72,7 @@ public class Encylcopedia extends AppCompatActivity {
                 overridePendingTransition(0, 0);
                 return true;
             } else if (item.getItemId() == R.id.settings_page) {
-                startActivity(new Intent(getApplicationContext(), settings_activity.class));
+                startActivity(new Intent(getApplicationContext(), Encyclopedia.class));
                 overridePendingTransition(0, 0);
                 return true;
             } else {
