d103f6961cf789f84facc2ea4ab62ff0c60c8d2f
Jake Powers <jpowers1324@gmail.com>
2023-04-20 01:45:35 -0400

    Functional Encyclopedia Page for Ability Scores
    (cherry picked from commit 98a89e409fc2a94b76d8257f7a570d6b778cd443)

--- app/src/main/java/com/example/bored_bard/encyclopedia/Encyclopedia.java
+++ app/src/main/java/com/example/bored_bard/encyclopedia/Encyclopedia.java
@@ -6,7 +6,10 @@ import android.os.Bundle;
 import android.util.Log;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.bored_bard.R;
 import com.example.bored_bard.UI_files.campaign_activity;
@@ -18,48 +21,56 @@ import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.DocumentChange;
 import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.EventListener;
 import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.FirebaseFirestoreException;
 import com.google.firebase.firestore.Query;
 import com.google.firebase.firestore.QueryDocumentSnapshot;
 import com.google.firebase.firestore.QuerySnapshot;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
+import com.google.gson.reflect.TypeToken;
 
 import org.w3c.dom.Document;
+
+import java.lang.reflect.Type;
+import java.util.ArrayList;
 
 public class Encyclopedia extends AppCompatActivity {
 
     private static final String TAG = "MyActivity";
+    RecyclerView recyclerView;
+    ArrayList<abilityScores> abilityScoresList;
+    MyAdapter myAdapter;
+    FirebaseFirestore db;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_encylcopedia); //Tie java file to xml file
-        FirebaseFirestore db = FirebaseFirestore.getInstance();
-        //Gson gson = new Gson();
+
+        recyclerView = findViewById(R.id.encyclopediaRecyclerView); //Initialize RecyclerView
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+
+        db = FirebaseFirestore.getInstance(); //Get Instance of Firestore
+        abilityScoresList = new ArrayList<abilityScores>();
+        myAdapter = new MyAdapter(Encyclopedia.this,abilityScoresList); //Initialize Adapter
+        recyclerView.setAdapter(myAdapter);
+        Gson gson = new GsonBuilder().create();
+
+        dbChange(gson);
+
+
 
 
         //CollectionReference ref = db.collection("Encyclopedia");
-        DocumentReference docRef = db.collection("Encyclopedia").document("Default/AbilityScores/cha");
-        /*db.collection("Encyclopedia").document("Default")
-                .get()
-                .addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
-                    @Override
-                    public void onComplete(@NonNull Task<DocumentSnapshot> task) {
-                        if(task.isSuccessful()) {
-                            DocumentSnapshot document = task.getResult();
-                            if( document.exists()) {
-                                Log.d(TAG, "DocumentSnapshot Data: " + document.getData());
-                            } else {
-                                Log.d(TAG, "No Such Document");
-                            }
-                        } else {
-                            Log.d(TAG, "Error getting documents: " + task.getException());
-                        }
-                    }
-                });*/
-        db.collection("Encyclopedia").document("Default").collection("AbilityScores")
+        //DocumentReference docRef = db.collection("Encyclopedia").document("Default");
+
+        //Calling and printing all documents in AbilityScores
+        /*db.collection("Encyclopedia").document("Default").collection("AbilityScores")
                 .get()
                 .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
                     @Override
@@ -72,7 +83,7 @@ public class Encyclopedia extends AppCompatActivity {
                             Log.d(TAG, "Error getting documents: ", task.getException());
                         }
                     }
-                });
+                });*/
 
 
 
@@ -107,7 +118,30 @@ public class Encyclopedia extends AppCompatActivity {
 
     }
 
+    private void dbChange(Gson gson) {
+        db.collection("Encyclopedia").document("Default").collection("AbilityScores").orderBy("name", Query.Direction.ASCENDING)
+                .addSnapshotListener(new EventListener<QuerySnapshot>() {
+                    @Override
+                    public void onEvent(@Nullable QuerySnapshot value, @Nullable FirebaseFirestoreException error) {
 
+                        if (error != null) {
+                            Log.e("Firestore Error",error.getMessage());
+                            return;
+                        }
+
+                        for (DocumentChange dc : value.getDocumentChanges()) {
+                            if (dc.getType() == DocumentChange.Type.ADDED) {
+                                Type listType = new TypeToken<ArrayList<abilityScores>>() {}.getType();
+                                //abilityScoresList.add(gson.fromJson(dc.getDocument().toString(),abilityScores.class));
+                                abilityScoresList.add(dc.getDocument().toObject(abilityScores.class));
+                            }
+
+                            myAdapter.notifyDataSetChanged();
+                        }
+                    }
+                });
+
+    }
 
 
 }
---
+++ app/src/main/java/com/example/bored_bard/encyclopedia/abilityScores.java
@@ -0,0 +1,79 @@
+package com.example.bored_bard.encyclopedia;
+
+import java.util.HashMap;
+import java.util.List;
+
+public class abilityScores {
+    private List<String> desc;
+    private String full_name;
+    private String index;
+    private String name;
+    private List<HashMap<String, String>> skills;
+    private String url;
+
+
+    public abilityScores(){};
+
+    public abilityScores(List<String> desc, String full_name, String index, String name, List<HashMap<String, String>> skills, String url) {
+        this.desc = desc;
+        this.full_name = full_name;
+        this.index = index;
+        this.name = name;
+        this.skills = skills;
+        this.url = url;
+    }
+
+    public String getDesc() {
+        String descString = "";
+
+        for (String i : desc) {
+            descString += i + "\n\n";
+        }
+        descString = descString.trim();
+        return descString;
+    }
+
+    public void setDesc(List<String> desc) {
+        this.desc = desc;
+    }
+
+    public String getFull_name() {
+        return full_name;
+    }
+
+    public void setFull_name(String full_name) {
+        this.full_name = full_name;
+    }
+
+    public String getIndex() {
+        return index;
+    }
+
+    public void setIndex(String index) {
+        this.index = index;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public List<HashMap<String,String>> getSkills() {
+        return skills;
+    }
+
+    public void setSkills(List<HashMap<String,String>> skills) {
+        this.skills = skills;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+}
